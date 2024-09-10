7db617de5a4af108595a33349ccf470af5d4266c
Muhammad Saif <muhammadsaif7717@gmail.com>
2024-09-10 23:21:34 +0600

    Navbar & Footer Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       Home
+      <Footer/>
     </div>
   );
 }
